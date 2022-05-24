c4e44cf90cf140c52ee9ff6c81281228316f93e8
KassandraSaint <kassandrastripper@gmail.com>
2022-05-24 10:56:36 -0400

    Add project files.

---
+++ LoadEventHandler/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace LoadEventHandler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.countriesBox = new System.Windows.Forms.ListBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(535, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Countries of South America";
+            // 
+            // countriesBox
+            // 
+            this.countriesBox.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countriesBox.FormattingEnabled = true;
+            this.countriesBox.ItemHeight = 36;
+            this.countriesBox.Location = new System.Drawing.Point(185, 86);
+            this.countriesBox.Name = "countriesBox";
+            this.countriesBox.Size = new System.Drawing.Size(424, 220);
+            this.countriesBox.TabIndex = 1;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft YaHei", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(275, 342);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(246, 84);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.countriesBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "South America";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox countriesBox;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ LoadEventHandler/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LoadEventHandler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Trying to read the text file and show contents in the List Box
+            try
+            {
+                // Initializing a variable to hold country name
+                string countryName;
+                // Initializing a StreamReader variable
+                StreamReader inputFile;
+
+                // Opening a text file and storing StreamReader object in inputFile variable
+                inputFile = File.OpenText("Countries.txt");
+
+                // Reading contents of the file, while it's not an end of a file
+                while (!inputFile.EndOfStream)
+                {
+                    // Reading a line in a file and storing it in the countryName variable
+                    countryName = inputFile.ReadLine();
+                    // Adding an item with the country name to the List Box 
+                    countriesBox.Items.Add(countryName);
+                }
+                // Closing the file
+                inputFile.Close();
+            }
+            // If an error occured while working with the file
+            catch (Exception ex)
+            {
+                // Printing an error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // Closing the form
+            this.Close();
+        }
+    }
+}
